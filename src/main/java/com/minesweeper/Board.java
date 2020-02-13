9f1ec4467753cbd8d3fdf2136e087b3844992d82
Fernando Petrola <fernando.petrola@avenida.com>
2020-02-12 22:29:59 -0300

    removing stringRepresentation property

--- src/main/java/com/minesweeper/Board.java
+++ src/main/java/com/minesweeper/Board.java
@@ -8,7 +8,6 @@ public class Board {
     private final List<Square> squares = new ArrayList<Square>();
     private final int rows;
     private final int columns;
-    private String stringRepresentation;
 
     public Board(String state, int rows, int columns) {
         this.rows = rows;
@@ -19,8 +18,6 @@ public class Board {
                 squares.add(new Square(row, column, getSquareValueAt(row, column, state)));
             }
         }
-
-        this.stringRepresentation = getStringRepresentation();
     }
 
     private String getStringRepresentation() {
@@ -28,12 +25,11 @@ public class Board {
     }
 
     public String toString() {
-        return stringRepresentation;
+        return getStringRepresentation();
     }
 
     public void hideSquares() {
         squares.stream().forEach(s -> s.hide());
-        stringRepresentation = getStringRepresentation();
     }
 
     public boolean thereIsMineAt(int row, int column) {
@@ -47,7 +43,5 @@ public class Board {
 
     public void showAll() {
         squares.stream().forEach(s -> s.show());
-
-        stringRepresentation = getStringRepresentation();
     }
 }

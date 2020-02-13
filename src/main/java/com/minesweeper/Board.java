55789d8c0daee42e196bab75698ce7b8cc03e33e
Fernando Petrola <fernando.petrola@avenida.com>
2020-02-12 22:01:14 -0300

    making it pass implementing showAll and stringrepresentation

--- src/main/java/com/minesweeper/Board.java
+++ src/main/java/com/minesweeper/Board.java
@@ -2,16 +2,30 @@ package com.minesweeper;
 
 public class Board {
     private String state;
+    private final int rows;
+    private final int columns;
+    private String stringRepresentation;
 
     public Board(String state, int rows, int columns) {
         this.state = state;
+        this.rows = rows;
+        this.columns = columns;
+        this.stringRepresentation = state;
     }
 
     public String toString() {
-        return state;
+        return stringRepresentation;
     }
 
     public void hideSquares() {
-        state= state.replace("X", ".");
+        stringRepresentation = state.replace("X", ".");
+    }
+
+    public boolean thereIsMineAt(int row, int column) {
+        return state.charAt(row * columns + column) == 'X';
+    }
+
+    public void showAll() {
+        stringRepresentation = state;
     }
 }
--- src/main/java/com/minesweeper/Game.java
+++ src/main/java/com/minesweeper/Game.java
@@ -1,11 +1,15 @@
 package com.minesweeper;
 
 public class Game {
-    public Game(Board board) {
+    private Board board;
 
+    public Game(Board board) {
+        this.board = board;
     }
 
     public void revealSquareAt(int row, int column) {
-
+        if (board.thereIsMineAt(row, column)) {
+            board.showAll();
+        }
     }
 }

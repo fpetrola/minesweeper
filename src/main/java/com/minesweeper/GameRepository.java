b78b69b61063fcd8fc2be9896ef6244b203710c3
Fernando Petrola <fernando.petrola@avenida.com>
2020-02-13 20:27:00 -0300

    implementing repository in memory

--- src/main/java/com/minesweeper/GameRepository.java
+++ src/main/java/com/minesweeper/GameRepository.java
@@ -1,11 +1,17 @@
 package com.minesweeper;
 
-public class GameRepository {
-    public void save(Game game) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class GameRepository {
+    private int id;
+    private Map<Integer, Game> games = new HashMap<>();
+
+    public void save(Game game) {
+        games.put(id++, game);
     }
 
     public Game findGameById(int id) {
-        return null;
+        return games.get(id);
     }
 }
